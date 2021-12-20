5b1fb4a533d37a3cd6e50f0b9ac2bd12733d48aa
SchwammPizza <ambros.anrig@gmail.com>
2021-12-20 14:58:53 +0100

    Flo_riana

--- Worlds/World1.gd
+++ Worlds/World1.gd
@@ -1,8 +1,13 @@
 extends Node2D
 
 func _ready():
-	for x in range(17):
-		for y in range(10):
+	for x in range(-54,54):
+		for y in range(-31,31):
 			if int(round(rand_range(0,3))) == 0:
 				if $FixMap.get_cell(x,y) == -1:
-					$RandomMap.set_cell(x,y,int(rand_range(1,32)))
+					$RandomMap.set_cell(x,y,int(rand_range(1,19)))
+				
+			elif int(round(rand_range(0,3))) == 0:
+				if $RandomMap.get_cell(x,y) == -1:
+					$RandomMap2.set_cell(x,y,int(rand_range(1,32)))
+
